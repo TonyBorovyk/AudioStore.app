8467df55601dc3b6dde37e899956f0d95bd93b01
Anton <antonbovik@gmail.com>
2021-04-22 09:17:55 +0300

    remuve playlists

--- back-end/api/controllers/playlists.controller.js
+++ back-end/api/controllers/playlists.controller.js
@@ -6,7 +6,10 @@ const {
 const getPlaylists = async (req, res) => {
   try {
     let playlists = await getPlaylistsService();
-
+    playlists = playlists.map((playlist) => ({
+      ...playlist,
+      tracks: playlist.tracks.map((songId) => getSongByIdService(songId)),
+    }));
     return res.send({
       data: playlists,
       success: true,
@@ -15,18 +18,21 @@ const getPlaylists = async (req, res) => {
     return {
       error,
       success: false,
-    }
+    };
   }
 };
 
 const getPlaylistById = async (req, res) => {
   try {
     let playlist = await getPlaylistByIdService(req.params.id);
-
+    playlists = playlists.map((playlist) => ({
+      ...playlist,
+      tracks: playlist.tracks.map((songId) => getSongByIdService(songId)),
+    }));
     return {
       data: playlist || {},
       success: true,
-    }
+    };
   } catch (error) {
     return res.send({
       error,
