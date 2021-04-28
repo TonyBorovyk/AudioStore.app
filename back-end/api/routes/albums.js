25905b07b61bbf1a10ba98daf442258af581ed1c
MykolaOnyshchuk <54044502+MykolaOnyshchuk@users.noreply.github.com>
2021-04-28 20:44:58 +0300

    Update

--- back-end/api/routes/albums.js
+++ back-end/api/routes/albums.js
@@ -1,4 +1,55 @@
-const { getAlbums, getAlbumById } = require('../controllers/albums.controller');
+const {
+  getAlbumsService,
+  getAlbumByIdService,
+} = require('../services/albums.service');
+const { getArtistByIdService } = require('../services/artists.service');
+const { getSongByIdService } = require('../services/songs.service');
+
+const getAlbums = async (req, res) => {
+  try {
+    let albums = await getAlbumsService();
+    albums = albums.map((album) => ({
+      ...album,
+      artists: album.artists.map((artistsId) =>
+        getArtistByIdService(artistsId)
+      ),
+      songs_list: album.songs_list.map((songId) => getSongByIdService(songId)),
+    }));
+
+    return {
+      data: albums,
+      success: true,
+    };
+  } catch (error) {
+    return res.send({
+      error,
+      success: false,
+    });
+  }
+};
+
+const getAlbumById = async (req, res) => {
+  try {
+    let album = await getAlbumByIdService(req.params.id);
+    album = {
+      ...album,
+      artists: album.artists.map((artistsId) =>
+        getArtistByIdService(artistsId)
+      ),
+      songs_list: album.songs_list.map((songId) => getSongByIdService(songId)),
+    };
+
+    return {
+      data: album || {},
+      success: true,
+    };
+  } catch (error) {
+    return res.send({
+      error,
+      success: false,
+    });
+  }
+};
 
 async function routes(fastify) {
   fastify.get('/', async (req, res) => {
