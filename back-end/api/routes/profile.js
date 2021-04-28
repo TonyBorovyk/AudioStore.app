84ede7671ac9f2eb938f2e07fd3dc116216e343b
Anton <antonbovik@gmail.com>
2021-04-28 23:12:58 +0300

    Fix

--- back-end/api/routes/profile.js
+++ back-end/api/routes/profile.js
@@ -3,32 +3,36 @@ require('dotenv').config();
 const { getUserById } = require('../services/profile.service');
 
 const getUser = async (req, res) => {
-  try {
+  const cookie = req.cookies.jwt;
+  if (cookie == undefined) {
+    res.code(401).send({ message: 'Unauthenticated', success: false });
+  }
+  const claims = jwt.verify(cookie, process.env.JWT_SECRET);
+  if (!claims) {
+    res.code(401).send({
+      message: 'Unauthenticated',
+      success: false,
+    });
+  }
+  const user = await getUserById(claims.id); // claims.id returns user id
+  res.send({ user, success: true });
+};
+
+async function routes(fastify) {
+  fastify.get('/', async (req, res) => {
     const cookie = req.cookies.jwt;
+    if (cookie == undefined) {
+      res.code(401).send({ message: 'Unauthenticated', success: false });
+    }
     const claims = jwt.verify(cookie, process.env.JWT_SECRET);
     if (!claims) {
-      return res.code(401).send({
+      res.code(401).send({
         message: 'Unauthenticated',
         success: false,
       });
     }
     const user = await getUserById(claims.id); // claims.id returns user id
-    return res.send({ user, success: true });
-  } catch (e) {
-    return res.code(401).send({
-      message: 'Unauthenticated',
-      success: false,
-    });
-  }
-};
-
-async function routes(fastify) {
-  fastify.get('/', async (req, res) => {
-    try {
-      res.send(await getUser(req, res));
-    } catch (e) {
-      res.send({ e });
-    }
+    res.send({ user, success: true });
   });
 }
 
